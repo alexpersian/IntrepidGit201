4e4b538a73d725552b07d5776f127170a0b6926b
Alex Persian <alex_persian@me.com>
2016-06-09 10:46:09 -0400

    Adds favorite things

---
+++ main.rb
@@ -0,0 +1,5 @@
+puts "Alex's Favorite Things:"
+puts "- Favorite Beer: Stone Delicious IPA"
+puts "- Favorite Movie: The Departed"
+puts "- Favorite Sweet: Liquorice"
+puts "- Favorite TV Show: Mr. Robot"
